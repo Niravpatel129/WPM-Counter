2e41f5725abf85c9ffb59107c8b0ac9e1a2bf1df
Nirav Patel <niravpatelp129@gmail.com>
2022-09-17 06:06:53 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'typing-speed-test';
+  demoTextArray =
+    `Hello, this is a sample text. I wanted to tell you that the weather is great today.`.split(
+      ' '
+    );
+
+  interval: any;
+
+  typedText = '';
+
+  timerInSeconds = 0;
+
+  getWPM = () => {
+    if (this.typedText.split(' ').length === this.demoTextArray.length) {
+      clearInterval(this.interval);
+    }
+
+    const correctWords = this.typedText
+      .split(' ')
+      .filter((word, index) => word === this.demoTextArray[index]).length;
+
+    const minutes = this.timerInSeconds / 60;
+    const wpm = correctWords / minutes;
+    return wpm || 0;
+  };
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.timerInSeconds++;
+    }, 1000);
+  }
+
+  doesTextMatch(str1: string, indexOfTypedText: number) {
+    if (!this.typedText.split(' ')[indexOfTypedText]) return 'NoMatchYet';
+    if (str1.length !== this.typedText.split(' ')[indexOfTypedText].length)
+      return 'NoMatchYet';
+
+    const correct = str1 === this.typedText.split(' ')[indexOfTypedText];
+
+    return correct ? 'Match' : 'NoMatch';
+  }
 }
